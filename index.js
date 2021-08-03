ff3f46e5605d9a1ab1f4c19b581a78229d238dbf
haki9975 <elias.robert.hakim@gmail.com>
2021-08-02 18:19:08 -0700

    Problem 2 solved

--- index.js
+++ index.js
@@ -10,19 +10,11 @@ function triangleLoop() {
 triangleLoop()
 
 function fuzzBizz() {
-    for (i = 0; i < 100; i++ ){
-        if (i % 3 == 0 ) {
-            console.log("Fizz")
-        }
-        else if (i % 5 == 0) {
-            console.log("Buzz")
-        }
-        else if (i % 5 == 0 && i % 3 == 0){
-            console.log("FuzzBuzz")
-        }
-        else {
-            console.log(i)
-        }
-        
+    for (i = 0; i <= 100; i++ ){
+        let output = ""
+        if (i % 3 == 0) {output += "Fizz";}
+        if ( i % 5 == 0) {output += "Buzz";}
+        console.log(output || i );
     }
 }
+fuzzBizz()
