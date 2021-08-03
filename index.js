c9d6fda5440ce551996b605454812a0b88394d5d
haki9975 <elias.robert.hakim@gmail.com>
2021-08-02 18:19:35 -0700

    Quick edit

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// document.addEventListener("DOMContentLoaded") => {
-//     console.log("hello")
-// }
 function triangleLoop() {
     for (let i = "#"; i.length < 8; i += "#") {
         console.log(i)
