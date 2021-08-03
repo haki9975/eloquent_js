3d14879581e18107b0c8b5806d7fc62ddfebe088
haki9975 <elias.robert.hakim@gmail.com>
2021-08-02 17:34:13 -0700

    Solved problem 1

---
+++ index.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded") => {
+//     console.log("hello")
+// }
+function triangleLoop() {
+    for (let i = "#"; i.length < 8; i += "#") {
+        console.log(i)
+    }
+}
+
+triangleLoop()
